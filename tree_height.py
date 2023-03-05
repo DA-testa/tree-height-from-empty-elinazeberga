f54de530cf35fcddc98d406f84dc9a2bd5483a22
elinazeberga <125008319+elinazeberga@users.noreply.github.com>
2023-03-05 23:34:04 +0000

    4

--- tree_height.py
+++ tree_height.py
@@ -6,64 +6,42 @@ import numpy
 
 
 def compute_height(n, parents):
-    max_height = 0
     augstumi = numpy.zeros(int(n))
     for i in range(int(n)):
-        if augstumi[i] > 0:
+        if augstumi[i] != 0:
             continue
-        augstums = 0
-        k=i
-        while k != -1:
-            if augstumi[k]>0:
-                augstums += augstumi[k]
+        augstums = 1
+        vertibas = parents[i]
+        while vertibas != -1:
+            if augstumi[vertibas] != 0:
+                augstums += augstumi[vertibas]
                 break
-            else:
-                augstums += 1
-                k = int(parents[i])
-                augstumi[k] = augstums
-        if augstums > max_height:
-            max_height = augstums
-    return max_height
+            augstums += 1 
+            vertibas = parents[vertibas]  
+        k = i
+        while k != -1 and augstumi[k] == 0:
+                augstumi[vertibas] = augstums
+                augstums -= 1
+                k = parents[k]
+    return numpy.max(augstumi)
 
-
-def input_file(file):
-    try:
-        with open(file,  encoding="utf8") as f:
-            saturs = f.readlines()
-    except IOError:
-        print("Error")
-        return None, None
-    
-    n=saturs[0].strip()
-    if n:
-        parents = saturs[1].strip().split(" ")
-        if parents:
-            f.close()
-            return n, parents
-    return None, None
-
-def ievadisana():
-    n = input().strip()
-    if n:
-        parents =  input().strip().split(" ")
-        if parents:
-            return n, parents
-    return None, None
 
 def main():
     ievadit = input().strip()
-    if ievadit == "F":
+    if ievadit == "I":
+        node = int(input().strip())
+        parents = numpy.array(list(map(int, input().split)))
+    else:
         file = input().strip()
-        if str(file[-1]) != "a":
-            n, parents = input_file(file)
-            if n and parents:
-                augstums = compute_height(n, parents)
-                print(int(augstums))
-    elif ievadit == "I":
-        n, parents = ievadisana()
-        if n and parents:
-            augstums = compute_height(n, parents)
-            print(int(augstums))
+        if "a" in file:
+            print("Error")
+            return
+        with open("test/" + file) as f:
+            node = int(f.readlines())    
+            parents = numpy.array(list(map(int, f.readline().split)))
+    augstums = compute_height(node, parents)
+    print(augstums)    
+    
 
     
     # implement input form keyboard and from files
