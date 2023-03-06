f7f3f3e40ec9bbd521fe6fde376167f2d6a6613a
elinazeberga <125008319+elinazeberga@users.noreply.github.com>
2023-03-06 01:00:52 +0000

    10

--- tree_height.py
+++ tree_height.py
@@ -6,38 +6,74 @@ import numpy
 
 
 def compute_height(n, parents):
-    augstumi = numpy.zeros(n)
-    def augstums(i):
-        if augstumi[i] != 0:
-            return augstumi[i]
-        if parents[i] == -1:
-            augstumi[i] = 1
-        else:
-            augstumi[i] = augstums(parents[i]) + 1
-        return augstumi[i]
+    augstums = 0
+    augstumi = []
+    s=0
+    augstumi = [i for i, e in enumerate(parents) if e == n]
+
+    if len(augstumi) == 0:
+        return 1
+    for f in augstumi:
+        augstums = max(compute_height(f, parents), augstums)
+    ##augstumi = numpy.zeros(n)
+    ##def augstums(i):
+        ##if augstumi[i] != 0:
+            ##return augstumi[i]
+        ##if parents[i] == -1:
+          ##  augstumi[i] = 1
+        ##else:
+          ##  augstumi[i] = augstums(parents[i]) + 1
+       ## return augstumi[i]
     
-    for i in range(n):
-        augstums(i)
-    return int(max(augstumi))
+    ##for i in range(n):
+      ##  augstums(i)
+    ##return int(max(augstumi))
        
 
 def main():
     check = input()
-    print(check)
     if "F" in check:
         file = input()
         if "a" not in file:
-            with open ("test/" + file, "r", encoding='UTF-8') as f:
-                ne = int(f.readline())
-                parent = list(map(int, f.readline().split()))
-        else:
             print("Error")
+        else:
+            izmantot = 'test/' + file
+            open_file = open(izmantot, 'r', encoding='UTF-8')
+            saturs = open_file.readlines()
+
+            koks = saturs[1]
+            daudzums = saturs[0]
     elif "I" in check:
-        ne = int(input())
-        parent = list(map(int, input().split()))
-    else:
-        print("Nav derīgs kods")
-    print(compute_height(ne, parent))
+    
+        koks = input()
+        daudzums = input()
+
+    koks = koks.split(" ")
+    koks = [int(i) for i in koks]
+    sakums = 0
+
+    p = 0
+    for k in koks:
+        if k == -1:
+            sakums = p
+        p = p+1
+
+    augstums = compute_height(sakums, koks)
+
+    print(augstums)
+ 
+
+          ##  with open ("test/" + file, "r", encoding='UTF-8') as f:
+            ##    ne = int(f.readline())
+              ##  parent = list(map(int, f.readline().split()))
+        ##else:
+          ##  print("Error")
+    ##
+      ##  ne = int(input())
+        ##parent = list(map(int, input().split()))
+    ##else:
+      ##  print("Nav derīgs kods")
+    ##print(compute_height(ne, parent))
         
 
     
